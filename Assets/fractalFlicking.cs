13345a11aa86a8173a928c2a3658a8b287d1672b
jeromeshan <43412578+jeromeshan@users.noreply.github.com>
2020-12-07 16:10:07 +0300

    new forest

--- Assets/fractalFlicking.cs
+++ Assets/fractalFlicking.cs
@@ -24,7 +24,7 @@ public class fractalFlicking : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
 
         if (Time.time > nextActionTime)
